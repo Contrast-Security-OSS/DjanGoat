99d4667a0820f6b3bac0007e935345f1dfd90dce
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-01 17:08:11 -0400

    reset database stuff

--- pygoat/settings.py
+++ pygoat/settings.py
@@ -71,16 +71,10 @@ WSGI_APPLICATION = 'pygoat.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'mydatabase',
-            'USER': 'username',
-            'PASSWORD': 'your_password',
-            'HOST': '',
-            'PORT': '',
-        }
-    }
+ 'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
 
 # Password validation
