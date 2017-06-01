b547250420092a542d380628e50f25a6d2411cad
Ryan Dens <ryandens@Ryans-MacBook-Pro.local>
2017-06-01 15:22:37 -0400

    Setup testing functionality. Included testing response for ok response code

--- app/tests.py
+++ app/tests.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from django.urls import reverse
+
 # Create your tests here.
+class BasicAppTest(TestCase):
+
+    def test_index(self):
+        response = self.client.get(reverse('app:index'))
+        self.assertEqual(response.status_code, 200)
