d1b15b4957f3af975c21f6b01ef0478a1c80d9f1
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-02 12:59:33 -0400

    Finished implementation of dashboard routes accomodating only the proper http requests

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,12 @@ app_name = "app"
 
 urlpatterns = [
     url(r'^$', views.app_index, name='index'),
-    url(r'^dashboard/$', views.dashboard_home, name='dashboard'),
+    url(r'^dashboard/$', views.dashboard_index, name='dashboard'),
+    url(r'^dashboard/home$', views.dashboard_home, name='dashboard home'),
+    url(r'^dashboard/doc$', views.dashboard_doc, name='dashboard doc'),
+    url(r'^dashboard/change_graph$', views.dashboard_change_graph, name='dashboard change graph'),
+    url(r'^dashboard/new$', views.dashboard_new, name='dashboard new'),
+    url(r'^dashboard/(?P<dashboard_id>[0-9]+)/edit$', views.dashboard_edit, name="dashboard edit"),
+    url(r'^dashboard/(?P<dashboard_id>[0-9]+)$', views.dashboard, name="dashboard"),
+
 ]
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -5,4 +5,11 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 
 from dashboard import home as dashboard_home
+from dashboard import doc as dashboard_doc
+from dashboard import index as dashboard_index
+from dashboard import change_graph as dashboard_change_graph
+from dashboard import new_dashboard as dashboard_new
+from dashboard import edit_dashboard as dashboard_edit
+from dashboard import dashboard as dashboard
+
 from index import index as app_index
