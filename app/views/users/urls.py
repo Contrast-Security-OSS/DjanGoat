f668e1db17a2e0f482c4c2fb85a129ae93bc628c
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-05 16:06:36 -0400

    Added routes, views, and unittests for the user message index, new user message, and edit user message. Tests check for route to exist and each view only accepting specified httpmethods

--- app/views/users/urls.py
+++ app/views/users/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^(?P<user_id>[0-9]+)/edit$', users_views.edit_user, name="user_edit"),
     url(r'^(?P<user_id>[0-9]+)/account_settings$', users_views.account_settings, name="user_account_settings"),
     url(r'^(?P<user_id>[0-9]+)$', users_views.user_view, name="user_view"),
-
+    url(r'^(?P<user_id>[0-9]+)/messages$', users_views.user_messages, name="user_messages"),
+    url(r'^(?P<user_id>[0-9]+)/messages/new$', users_views.new_user_message, name="new_user_message"),
+    url(r'^(?P<user_id>[0-9]+)/messages/(?P<message_id>[0-9]+)/edit$', users_views.edit_user_message, name="edit_user_message"),
 ]
--- app/views/users/views.py
+++ app/views/users/views.py
@@ -26,7 +26,18 @@ def edit_user(request, user_id):
 def account_settings(request, user_id):
     return HttpResponse("Account settings for user #" + str(user_id))
 
-
 @require_http_methods(["GET", "PATCH", "PUT", "DELETE"])
 def user_view(request, user_id):
     return HttpResponse("User " + str(user_id) + " " + str(request.method))
+
+@require_http_methods(["GET", "POST"])
+def user_messages(request, user_id):
+    return HttpResponse("User " + str(user_id) + " messages index")
+
+@require_http_methods(["GET"])
+def new_user_message(request, user_id):
+    return HttpResponse("New message for user " + str(user_id))
+
+@require_http_methods(["GET"])
+def edit_user_message(request, user_id, message_id):
+    return HttpResponse("Edit message " + str(message_id) + " for user " + str(user_id))
