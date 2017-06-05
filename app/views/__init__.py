0577e085b7f5263251e0ed0d3f4e045c0f44a474
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-05 11:32:08 -0400

    removed comments and redid naming

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -5,6 +5,3 @@ from index import index as app_index
 from dashboard import views as dashboard_views
 
 from tutorials import views as tutorials_views
-
-# from tutorials import urls as tutorial_urls
-# from tutorials.views import *
