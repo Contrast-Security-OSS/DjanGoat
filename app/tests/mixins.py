a90db132bbe19b4a830b6a94a83fefe121bc213d
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-05 18:27:24 -0400

    created mixin refactored tutorials testing. need to find way to override

---
+++ app/tests/mixins.py
@@ -0,0 +1,60 @@
+from django.test import TestCase, RequestFactory, Client
+
+from django.urls import reverse
+
+
+# Mixin for testing the creation of routes
+# HTTP request objects should be in the form [GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS, TRACE]
+
+class RouteTestingMixin(object):
+    def test_route_exists(self):
+        response = self.client.get(reverse(self.route_name))
+        self.assertEqual(response.status_code, 200)
+
+    def test_route_get(self):
+        request = self.factory.get(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[0],
+                         "your get method for route: " + self.route + " returned a different status code")
+
+    def test_route_post(self):
+        request = self.factory.post(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[1],
+                         "your post method for route: " + self.route + "returned a different status code")
+
+    def test_route_put(self):
+        request = self.factory.put(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[2],
+                         "your put method for route: " + self.route + "returned a different status code")
+
+    def test_route_patch(self):
+        request = self.factory.patch(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[3],
+                         "your patch method for route: " + self.route + "returned a different status code")
+
+    def test_route_delete(self):
+        request = self.factory.delete(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[4],
+                         "your delete method for route: " + self.route + "returned a different status code")
+
+    def test_route_head(self):
+        request = self.factory.head(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[5],
+                         "your head method for route: " + self.route + "returned a different status code")
+
+    def test_route_options(self):
+        request = self.factory.options(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[6],
+                         "your options method for route: " + self.route + "returned a different status code")
+
+    def test_route_trace(self):
+        request = self.factory.trace(self.route)
+        response = self.view(request)
+        self.assertEqual(response.status_code, self.responses[7],
+                         "your options method for route: " + self.route + "returned a different status code")
--- app/tests/views/test_tutorials.py
+++ app/tests/views/test_tutorials.py
@@ -4,103 +4,31 @@ from __future__ import unicode_literals
 from django.test import TestCase, RequestFactory, Client
 
 from django.urls import reverse
+from .. import mixins
 
 import app.views as views
 
 tutorials = views.tutorials_views
 
 
-class TutorialTestsIndex(TestCase):
-    # setup for all test cases
+class TutorialTestsIndex(TestCase, mixins.RouteTestingMixin):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-
-    # Verifies the route exists by getting the dashboard index
-    # and ensuring the response code is 200 (OK)
-    def test_dashboard_index_route_exists(self):
-        response = self.client.get(reverse('app:tutorials_index'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_get(self):
-        request = self.factory.get('/tutorials')
-        response = tutorials.index(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_dashboard_post(self):
-        request = self.factory.post('/tutorials')
-        response = tutorials.index(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_dashboard_put(self):
-        request = self.factory.put('/tutorials')
-        response = tutorials.index(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_dashboard_delete(self):
-        request = self.factory.delete('/tutorials')
-        response = tutorials.index(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_dashboard_head(self):
-        request = self.factory.head('/tutorials')
-        response = tutorials.index(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_dashboard_options(self):
-        request = self.factory.options('/tutorials')
-        response = tutorials.index(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_dashboard_trace(self):
-        request = self.factory.trace('/tutorials')
-        response = tutorials.index(request)
-        self.assertEqual(response.status_code, 405)
+        self.route_name = 'app:tutorials_index'
+        self.route = '/tutorial'
+        self.view = tutorials.index
+        self.responses = [200, 200, 405, 405, 405, 405, 405, 405]
 
 
-class TutorialsCredentialsTests(TestCase):
+class TutorialsCredentialsTests(TestCase, mixins.RouteTestingMixin):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-
-    def test_dashboard_index_route_exists(self):
-        response = self.client.get(reverse('app:tutorials_credentials'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_credentials_get(self):
-        request = self.factory.get('/tutorials/credentials')
-        response = tutorials.credentials(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_credentials_post(self):
-        request = self.factory.post('/tutorials/credentials')
-        response = tutorials.credentials(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_credentials_put(self):
-        request = self.factory.put('/tutorials/credentials')
-        response = tutorials.credentials(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_credentials_delete(self):
-        request = self.factory.delete('/tutorials/credentials')
-        response = tutorials.credentials(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_credentials_head(self):
-        request = self.factory.head('/tutorials/credentials')
-        response = tutorials.credentials(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_credentials_options(self):
-        request = self.factory.options('/tutorials/credentials')
-        response = tutorials.credentials(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_credentials_trace(self):
-        request = self.factory.trace('/tutorials/credentials')
-        response = tutorials.credentials(request)
-        self.assertEqual(response.status_code, 405)
+        self.route_name = 'app:tutorials_credentials'
+        self.route = '/tutorials/credentials'
+        self.view = tutorials.credentials
+        self.responses = [200, 405, 405, 405, 405, 405, 405, 405]
 
 
 class TutorialsIdTests(TestCase):
@@ -210,47 +138,7 @@ class TutorialsNewTests(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-
-    def test_dashboard_index_route_exists(self):
-        response = self.client.get(reverse('app:tutorials_new'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_edit_id_get(self):
-        request = self.factory.get('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 200)
-
-    def test_edit_id_post(self):
-        request = self.factory.post('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_edit_id_put(self):
-        request = self.factory.put('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_edit_id_delete(self):
-        request = self.factory.delete('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_edit_id_head(self):
-        request = self.factory.head('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_edit_id_options(self):
-        request = self.factory.options('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_edit_id_trace(self):
-        request = self.factory.trace('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 405)
-
-    def test_edits_id_patch(self):
-        request = self.factory.patch('/tutorials/new')
-        response = tutorials.new(request)
-        self.assertEqual(response.status_code, 405)
+        self.route_name = 'app:tutorials_new'
+        self.route = '/tutorials/new'
+        self.view = tutorials.new
+        self.responses = [200, 200, 405, 405, 405, 405, 405, 405]
