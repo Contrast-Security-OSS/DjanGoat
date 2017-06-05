2ced07ddc5db3c9456ced1c56a65a1f8c752223d
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-05 11:42:01 -0400

    removed a star

--- app/tests/models/test_model_note.py
+++ app/tests/models/test_model_note.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase, RequestFactory, Client
 
-from app.models import *
+from app.models import Note
 
 from django.utils import timezone
 
