d22fbc4b2d9f88e662a6583167f1b9851e8ae499
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-05 14:28:25 -0400

    added routes for users views with basic templates inside

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from index import index as app_index
 
 from dashboard import views as dashboard_views
-
 from tutorials import views as tutorials_views
+from users import views as users_views
