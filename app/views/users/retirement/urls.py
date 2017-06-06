ad1feacbdd3db76c30c5db172484997ecee02209
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-06 09:59:30 -0400

    added routes, views, and automated testing for show user retirement

--- app/views/users/retirement/urls.py
+++ app/views/users/retirement/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', retirement_views.user_retirement_index, name="user_retirement_index"),
     url(r'^new$', retirement_views.new_user_retirement, name="new_user_retirement"),
     url(r'^(?P<retirement_id>[0-9]+)/edit$', retirement_views.edit_user_retirement, name="edit_user_retirement"),
-    # url(r'^(?P<message_id>[0-9]+)$', retirement_views.user_retirement, name="user_retirement"),
+    url(r'^(?P<retirement_id>[0-9]+)$', retirement_views.user_retirement, name="user_retirement"),
 ]
