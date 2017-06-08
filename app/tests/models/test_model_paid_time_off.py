8ee84875a8da648b56a109fac9d89ea6f61f4680
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-08 17:03:11 -0400

    added CRUD tests for pto model

--- app/tests/models/test_model_paid_time_off.py
+++ app/tests/models/test_model_paid_time_off.py
@@ -5,10 +5,10 @@ from django.test import TestCase
 import datetime
 import pytz
 from app.models import User
-from app.models import KeyManagement
+from app.models import PaidTimeOff
 
 
-class KeyManagementModelTests(TestCase):
+class UserModelTests(TestCase):
 
     user = None
     input_email = None
@@ -41,52 +41,60 @@ class KeyManagementModelTests(TestCase):
         )
         self.user.save()
 
-        #Create KeyManagement Model
-        self.input_iv = "my_iv"
-        self.km_input_create_date = pytz.utc.localize(datetime.datetime(2017, 6, 4, 0, 0))
-        self.km_input_update_date = pytz.utc.localize(datetime.datetime(2017, 6, 5, 0, 0))
+        #Create PTO Model
+        self.input_sick_days_earned = 20
+        self.input_sick_days_taken = 2
+        self.input_pto_days_earned = 10
+        self.input_pto_days_taken = 4
+        self.pto_input_create_date = pytz.utc.localize(datetime.datetime(2017, 6, 4, 0, 0))
+        self.pto_input_update_date = pytz.utc.localize(datetime.datetime(2017, 6, 5, 0, 0))
 
-        self.key_management = KeyManagement.objects.create(
-            iv=self.input_iv, user_id = self.user,
-            created_at=self.km_input_create_date,
-            updated_at=self.km_input_update_date
+        self.pto = PaidTimeOff.objects.create(
+            sick_days_earned=self.input_sick_days_earned,
+            sick_days_taken=self.input_sick_days_taken,
+            pto_earned=self.input_pto_days_earned,
+            pto_taken=self.input_pto_days_taken,
+            created_at=self.pto_input_create_date,
+            updated_at=self.pto_input_update_date,
+            user_id=self.user
         )
-        self.key_management.save()
-        self.db_key_management = KeyManagement.objects.filter(iv=self.input_iv).first()
 
-    def test_create_key_management(self):
-        self.assertNotEqual(self.db_key_management, None)
+        self.pto.save()
+        self.db_pto = PaidTimeOff.objects.filter(pk=self.pto.pk).first()
 
-    def test_read_key_management(self):
-        self.assertEqual(self.db_key_management, self.key_management)
+    def test_create_pto(self):
+        self.assertNotEqual(self.db_pto, None)
 
-    def test_update_key_management(self):
-        self.key_management.iv = "my_iv2"
-        self.key_management.save()
-        self.db_key_management = KeyManagement.objects.filter(pk=self.key_management.pk).first()
-        self.assertEqual(self.key_management.iv, self.db_key_management.iv)
+    def test_read_pto(self):
+        self.assertEqual(self.db_pto, self.pto)
 
-    def test_delete_key_management(self):
-        response = self.key_management.delete()
+    def test_update_pto(self):
+        self.pto.sick_days_taken = 5
+        self.pto.save()
+        self.db_pto = PaidTimeOff.objects.filter(pk=self.pto.pk).first()
+        self.assertEqual(self.pto.sick_days_taken, self.db_pto.sick_days_taken)
+
+    def test_delete_pto(self):
+        response = self.pto.delete()
         num_objects_deleted = response[0]
         '''1 object deleted from database'''
         self.assertEqual(num_objects_deleted, 1)
 
-        self.db_key_management = KeyManagement.objects.filter(pk=self.key_management.pk).first()
-        '''Key management with primary key matching the deleted key management's primary key no longer in database'''
-        self.assertEqual(self.db_key_management, None)
+        self.db_pto = PaidTimeOff.objects.filter(pk=self.pto.pk).first()
+        '''PTO with primary key matching the deleted pto's primary key no longer in database'''
+        self.assertEqual(self.db_pto, None)
 
-    def test_delete_user_and_key_management(self):
-        """Test to ensure key management is deleted when its user is deleted.
+    def test_delete_user_and_pto(self):
+        """Test to ensure pto is deleted when its user is deleted.
         """
         response = self.user.delete()
         num_objects_deleted = response[0]
         '''2 objects deleted from database'''
         self.assertEqual(num_objects_deleted, 2)
 
-        self.db_key_management = KeyManagement.objects.filter(pk=self.key_management.pk).first()
+        self.db_pto = PaidTimeOff.objects.filter(pk=self.pto.pk).first()
         db_user = User.objects.filter(pk=self.user.pk).first()
-        self.assertIsNone(self.db_key_management, "Key Management not deleted from database")
+        self.assertIsNone(self.db_pto, "PTO not deleted from database")
         self.assertIsNone(db_user, "User not deleted from the database")
 
 
