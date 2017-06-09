5cf09ec49e827eb13b254e69b7072625e02135a5
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-09 09:55:24 -0400

    upated key management tests to use mixin

--- app/tests/models/test_model_key_management.py
+++ app/tests/models/test_model_key_management.py
@@ -6,87 +6,47 @@ import datetime
 import pytz
 from app.models import User
 from app.models import KeyManagement
+from app.tests.mixins import ModelCrudTests
 
 
-class KeyManagementModelTests(TestCase):
-
-    user = None
-    input_email = None
-    input_password = None
-    input_admin = None
-    input_first_name = None
-    input_last_name = None
-    u_input_create_date = None
-    u_input_update_date = None
-    input_auth_token = None
+class KeyManagementModelTests(TestCase, ModelCrudTests):
 
     def setUp(self):
-        self.input_user_id = 1
-        self.input_email = "ryan.dens@contrastsecurity.com"
-        self.input_password = "12345"
-        self.input_admin = True
-        self.input_first_name = "Ryan"
-        self.input_last_name = "Dens"
-        self.u_input_create_date = datetime.datetime(2017, 6, 1, 0, 0)
-        self.u_input_create_date = pytz.utc.localize(self.u_input_create_date)
-        self.u_input_update_date = datetime.datetime(2017, 6, 3, 0, 0)
-        self.u_input_update_date = pytz.utc.localize(self.u_input_update_date)
-        self.input_auth_token = "test"
-        self.user = User.objects.create(
-            user_id=self.input_user_id,
-            email=self.input_email, password=self.input_password,
-            is_admin=self.input_admin, first_name=self.input_first_name,
-            last_name=self.input_last_name, created_at=self.u_input_create_date,
-            updated_at=self.u_input_update_date, auth_token=self.input_auth_token
+
+        # Create the user
+        input_user_id = 1
+        input_email = "ryan.dens@contrastsecurity.com"
+        input_password = "12345"
+        input_admin = True
+        input_first_name = "Ryan"
+        input_last_name = "Dens"
+        u_input_create_date = pytz.utc.localize(datetime.datetime(2017, 6, 1, 0, 0))
+        u_input_update_date = pytz.utc.localize(datetime.datetime(2017, 6, 3, 0, 0))
+        input_auth_token = "test"
+
+        self.parent = User.objects.create(
+            user_id=input_user_id,
+            email=input_email, password=input_password,
+            is_admin=input_admin, first_name=input_first_name,
+            last_name=input_last_name, created_at=u_input_create_date,
+            updated_at=u_input_update_date, auth_token=input_auth_token
         )
-        self.user.save()
+        self.parent.save()
 
-        #Create KeyManagement Model
-        self.input_iv = "my_iv"
-        self.km_input_create_date = pytz.utc.localize(datetime.datetime(2017, 6, 4, 0, 0))
-        self.km_input_update_date = pytz.utc.localize(datetime.datetime(2017, 6, 5, 0, 0))
+        # Create KeyManagement Model
+        input_iv = "my_iv"
+        km_input_create_date = pytz.utc.localize(datetime.datetime(2017, 6, 4, 0, 0))
+        km_input_update_date = pytz.utc.localize(datetime.datetime(2017, 6, 5, 0, 0))
 
-        self.key_management = KeyManagement.objects.create(
-            iv=self.input_iv, user_id = self.user,
-            created_at=self.km_input_create_date,
-            updated_at=self.km_input_update_date
+        self.model = KeyManagement.objects.create(
+            iv=input_iv, user_id = self.parent,
+            created_at=km_input_create_date,
+            updated_at=km_input_update_date
         )
-        self.key_management.save()
-        self.db_key_management = KeyManagement.objects.filter(iv=self.input_iv).first()
+        self.model.save()
 
-    def test_create_key_management(self):
-        self.assertNotEqual(self.db_key_management, None)
+        self.model_update_attribute = "iv"
+        self.model_update_input = "iv2"
 
-    def test_read_key_management(self):
-        self.assertEqual(self.db_key_management, self.key_management)
-
-    def test_update_key_management(self):
-        self.key_management.iv = "my_iv2"
-        self.key_management.save()
-        self.db_key_management = KeyManagement.objects.filter(pk=self.key_management.pk).first()
-        self.assertEqual(self.key_management.iv, self.db_key_management.iv)
-
-    def test_delete_key_management(self):
-        response = self.key_management.delete()
-        num_objects_deleted = response[0]
-        '''1 object deleted from database'''
-        self.assertEqual(num_objects_deleted, 1)
-
-        self.db_key_management = KeyManagement.objects.filter(pk=self.key_management.pk).first()
-        '''Key management with primary key matching the deleted key management's primary key no longer in database'''
-        self.assertEqual(self.db_key_management, None)
-
-    def test_delete_user_and_key_management(self):
-        """Test to ensure key management is deleted when its user is deleted.
-        """
-        response = self.user.delete()
-        num_objects_deleted = response[0]
-        '''2 objects deleted from database'''
-        self.assertEqual(num_objects_deleted, 2)
-
-        self.db_key_management = KeyManagement.objects.filter(pk=self.key_management.pk).first()
-        db_user = User.objects.filter(pk=self.user.pk).first()
-        self.assertIsNone(self.db_key_management, "Key Management not deleted from database")
-        self.assertIsNone(db_user, "User not deleted from the database")
 
 
