03f15de320df7676859b5fdbd3ea447f460d9957
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-09 10:56:40 -0400

    CONTRAST-14702 added tests for performance model

--- app/admin.py
+++ app/admin.py
@@ -4,6 +4,16 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from models import Note
+from models import User
+from models import PaidTimeOff
+from models import KeyManagement
+from models import Pay
+from models import Performance
 
 # Register your models here.
 admin.site.register(Note)
+admin.site.register(User)
+admin.site.register(PaidTimeOff)
+admin.site.register(KeyManagement)
+admin.site.register(Pay)
+admin.site.register(Performance)
