406c87cb1abc3f3cd5779f43aed4ed15db829d06
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-09 10:05:34 -0400

    Created and migrated the Pay model

---
+++ app/models/Pay/pay.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.core.validators import MaxValueValidator
+
+
+@python_2_unicode_compatible
+class Pay(models.Model):
+    """
+    Class defining the Pay model
+
+    t.integer  "user_id",            limit: 4
+    t.string   "bank_account_num",   limit: 255
+    t.string   "bank_routing_num",   limit: 255
+    t.integer  "percent_of_deposit", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    """
+    MAX_INT_VALUE = 2**32-1
+
+
+    def __str__(self):
+        return self.user_id.__str__() + " Pay Summary: \n" \
+               + "\nBank Account Number: " + self.bank_account_num \
+               + "\nBank Routing Number: " + self.bank_routing_num \
+               + "\nPercent of deposit: " + str(self.percent_of_deposit)
+
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    bank_account_num = models.CharField(max_length=255)
+    bank_routing_num = models.CharField(max_length=255)
+    percent_of_deposit = models.PositiveIntegerField(validators=[MaxValueValidator(MAX_INT_VALUE)])
+    created_at = models.DateTimeField('date created')
+    updated_at = models.DateTimeField('date updated')
+
+    class Meta:
+        db_table = "app_pays"
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.Note.note import Note
 from app.models.User.user import User
 from app.models.Key_Managements.key_management import KeyManagement
 from app.models.PaidTimeOff.paid_time_off import PaidTimeOff
+from app.models.Pay.pay import Pay
