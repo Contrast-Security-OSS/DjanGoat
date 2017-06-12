5457ed4443d47e44b35d11363c34f3f892f2a0db
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-12 10:49:00 -0400

    Constructed templates for messages view nested under users

--- app/views/users/messages/views.py
+++ app/views/users/messages/views.py
@@ -2,25 +2,32 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.template.loader import get_template
 
 
 @require_http_methods(["GET", "POST"])
 def user_messages(request, user_id):
-    return HttpResponse("User " + str(user_id) + " messages index")
+    t = get_template('users/messages/index.html')
+    c = {'user_id': user_id}
+    return HttpResponse(t.render(c, request))
 
 
 @require_http_methods(["GET"])
 def new_user_message(request, user_id):
-    return HttpResponse("New message for user " + str(user_id))
+    t = get_template('users/messages/new.html')
+    c = {'user_id': user_id}
+    return HttpResponse(t.render(c, request))
 
 
 @require_http_methods(["GET"])
 def edit_user_message(request, user_id, message_id):
-    return HttpResponse("Edit message " + str(message_id) + " for user " +
-                        str(user_id))
+    t = get_template('users/messages/edit.html')
+    c = {'user_id': user_id, 'message_id': message_id}
+    return HttpResponse(t.render(c, request))
 
 
 @require_http_methods(["GET", "PATCH", "PUT", "DELETE"])
 def user_message(request, user_id, message_id):
-    return HttpResponse("View message " + str(message_id) + " for user " +
-                        str(user_id))
+    t = get_template('users/messages/show.html')
+    c = {'user_id': user_id, 'message_id': message_id}
+    return HttpResponse(t.render(c, request))
