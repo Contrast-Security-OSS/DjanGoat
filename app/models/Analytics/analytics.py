9e8ead89e3c20b8d660bc51b1d3e3bce8952e830
Ziyang Wang <ziyangwang@Ziyangs-MacBook-Pro.local>
2017-06-12 16:14:16 -0400

    added methods on analytics model, and relevant tests

--- app/models/Analytics/analytics.py
+++ app/models/Analytics/analytics.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+from django.db.models import Count
 
 
 @python_2_unicode_compatible
@@ -19,3 +20,27 @@ class Analytics(models.Model):
     user_agent = models.CharField(max_length=255)
     created_at = models.DateTimeField('date created')
     updated_at = models.DateTimeField('date updated')
+
+    @classmethod
+    def hits_by_ip(cls, ip, col='*'):
+        # raw method requires a primary key
+        if (col != '*'):
+            col = 'id, ' + col
+        table_name = cls.objects.model._meta.db_table
+        return cls.objects.raw(
+            "SELECT %s FROM %s WHERE ip_address='%s' ORDER BY id DESC"
+            % (col, table_name, ip))
+
+    # defined in railsgoat but not used, expects valid column name
+    @classmethod
+    def count_by_col(cls, col):
+        return cls.objects.values(col).count()
+
+    # expects field type to be string
+    @classmethod
+    def parse_field(cls, field):
+        valid_fields = ["ip_address", "referrer", "user_agent"]
+        if field in valid_fields:
+            return field
+        else:
+            return '1'
