b6f682708de4599b49a00b7d65f244adacb29d65
Ziyang Wang <ziyangwang@Ziyangs-MacBook-Pro.local>
2017-06-12 16:16:26 -0400

    removed unnecessary imports

--- app/models/Analytics/analytics.py
+++ app/models/Analytics/analytics.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
-from django.db.models import Count
 
 
 @python_2_unicode_compatible
