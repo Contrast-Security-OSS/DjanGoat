c0688fbe6f363d1ddf2dbed2041e4b731990fa84
Justin Leo <justin.leo@contrastsecurity.com>
2017-06-13 13:47:24 -0400

    [CONTRAST-14553] reverted prod settings

--- pygoat/production_settings.py
+++ pygoat/production_settings.py
@@ -1,11 +1,9 @@
-from settings import *
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pygoat',
-        'USER': 'pygoat',
-        'PASSWORD': 'default1!',
+        'NAME': 'INSERT_NAME',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',  # can leave blank (sets to default)
         'PORT': '',  # can leave blank (sets to default)
     }
