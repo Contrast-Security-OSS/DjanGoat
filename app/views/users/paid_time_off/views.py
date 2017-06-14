13a0c5627eea9dbb349127336092c2b2dcc12527
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-14 15:33:47 -0400

    fixed the bug that was causing key to be passed in not the actual value

--- app/views/users/paid_time_off/views.py
+++ app/views/users/paid_time_off/views.py
@@ -6,20 +6,21 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET", "POST"])
 def index(request, user_id):
-    return HttpResponse("Paid time off index for user " + user_id)
+    return HttpResponse("Paid time off index for user " + str(user_id))
 
 
 @require_http_methods(["GET"])
 def new(request, user_id):
-    return HttpResponse("You made a new pto for user " + user_id)
+    return HttpResponse("You made a new pto for user " + str(user_id))
 
 
 @require_http_methods(["GET"])
 def edit(request, user_id, id):
-    return HttpResponse("Edit PTO for user " + user_id + " with id " + id)
+    return HttpResponse("Edit PTO for user " + str(user_id) +
+                        " with id " + str(id))
 
 
 @require_http_methods(["GET", "PATCH", "DELETE", "PUT"])
 def pto_id(request, user_id, id):
-    return HttpResponse("Show, update, or destroy PTO for user " + user_id +
-                        " with id " + id)
+    return HttpResponse("Show, update, or destroy PTO for user " +
+                        str(user_id) + " with id " + str(id))
--- app/views/users/pay/views.py
+++ app/views/users/pay/views.py
@@ -6,31 +6,31 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["POST"])
 def update_dd_info(request, user_id):
-    return HttpResponse("Update dd info for user " + user_id)
+    return HttpResponse("Update dd info for user " + str(user_id))
 
 
 @require_http_methods(["POST"])
 def decrypt_bank_acct_num(request, user_id):
-    return HttpResponse("Decrypt the bank info " + user_id)
+    return HttpResponse("Decrypt the bank info " + str(user_id))
 
 
 @require_http_methods(["GET", "POST"])
 def user_pay_index(request, user_id):
-    return HttpResponse("Pay index for user " + user_id)
+    return HttpResponse("Pay index for user " + str(user_id))
 
 
 @require_http_methods(["GET"])
 def new_user_pay(request, user_id):
-    return HttpResponse("New pay for " + user_id)
+    return HttpResponse("New pay for " + str(user_id))
 
 
 @require_http_methods(["GET"])
 def edit_user_pay(request, user_id, id):
-    return HttpResponse("Edit pay for user " + user_id +
-                        " for person with id " + id)
+    return HttpResponse("Edit pay for user " + str(user_id) +
+                        " for person with id " + str(id))
 
 
 @require_http_methods(["GET", "PATCH", "PUT", "DELETE"])
 def user_pay(request, user_id, id):
-    return HttpResponse("Pay for user " + user_id +
-                        " for person with id " + id)
+    return HttpResponse("Pay for user " + str(user_id) +
+                        " for person with id " + str(id))
