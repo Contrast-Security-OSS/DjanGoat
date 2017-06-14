a627a040a5f5c95b7800cd5b72967eed8f3c4604
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-14 15:49:45 -0400

    CONTRAST-14964 created regression tests file that includes short test of the file

---
+++ app/tests/test_regressions_tests.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+from django.test import TestCase, RequestFactory, Client
+
+from app.tests.mixins import RouteTestingWithKwargs
+
+from app.views import user_retirement_views
+
+
+class PassingWrongArgumentRegressionTests(TestCase, RouteTestingWithKwargs):
+    def setUp(self):
+        """ Class for testing api index view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:user_retirement_index'
+        self.route = '/users/55/retirement'
+        self.view = user_retirement_views.user_retirement_index
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 200,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'user_id': 55}
+
+    def dummy_function(self, user_id):
+        return type(user_id) is int
+
+    def test_kwarg_type_is_correct(self):
+        self.assertTrue(self.dummy_function(**self.kwargs))
