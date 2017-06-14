a52c25b6359e263d0bd836155f72642e7bfe7dc9
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-13 23:58:44 -0400

    added tests

---
+++ app/tests/views/test_admin.py
@@ -0,0 +1,245 @@
+from __future__ import unicode_literals
+from django.test import TestCase, RequestFactory, Client
+from app.tests.mixins import RouteTestingWithKwargs
+from django.urls import reverse
+import app.views.admin.views as admin_views
+
+
+class AdminIndexTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin index view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin_index'
+        self.route = '/admin'
+        self.view = admin_views.admin_index
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 200,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {}
+
+
+class AdminNewTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin new view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:new_admin'
+        self.route = '/admin/new'
+        self.view = admin_views.new_admin
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 405,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {}
+
+
+class AdminEditTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin edit view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:edit_admin'
+        self.route = '/admin/5/edit'
+        self.view = admin_views.edit_admin
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 405,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'id': 5}
+
+
+class AdminIDTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin  view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin'
+        self.route = '/admin/5/'
+        self.view = admin_views.admin
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 405,
+            'PUT': 200,
+            'PATCH': 200,
+            'DELETE': 200,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'id': 5}
+
+
+class AdminDashboardTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin dashboard view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin_dashboard'
+        self.route = '/admin/5/dashboard'
+        self.view = admin_views.admin_dashboard
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 405,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'admin_id': 5}
+
+
+class AdminGetUserTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin dashboard view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin_get_user'
+        self.route = '/admin/5/get_user'
+        self.view = admin_views.admin_get_user
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 405,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'admin_id': 5}
+
+
+class AdminDeleteUserTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin delete view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin_delete_user'
+        self.route = '/admin/5/delete_user'
+        self.view = admin_views.admin_delete_user
+        self.responses = {
+            'exists': 200,
+            'GET': 405,
+            'POST': 200,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'admin_id': 5}
+
+    # Verifies a route exists
+    def test_route_exists(self):
+        response = self.client.post(reverse(self.route_name, kwargs=self.kwargs))
+        self.assertEqual(response.status_code, self.responses['exists'])
+
+
+class AdminUpdateUserTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin delete view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin_update_user'
+        self.route = '/admin/5/update_user'
+        self.view = admin_views.admin_update_user
+        self.responses = {
+            'exists': 200,
+            'GET': 405,
+            'POST': 405,
+            'PUT': 405,
+            'PATCH': 200,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'admin_id': 5}
+
+    # Verifies a route exists
+    def test_route_exists(self):
+        response = self.client.patch(reverse(self.route_name, kwargs=self.kwargs))
+        self.assertEqual(response.status_code, self.responses['exists'])
+
+
+class AdminGetAllUsersTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin delete view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin_get_all_users'
+        self.route = '/admin/5/get_all_users'
+        self.view = admin_views.admin_get_all_users
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 405,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'admin_id': 5}
+
+
+class AdminAnalyticsUsersTest(TestCase, RouteTestingWithKwargs):
+    # setup for all test cases
+    def setUp(self):
+        """ Class for testing admin delete view"""
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:admin_analytics'
+        self.route = '/admin/5/analytics'
+        self.view = admin_views.admin_analytics
+        self.responses = {
+            'exists': 200,
+            'GET': 200,
+            'POST': 405,
+            'PUT': 405,
+            'PATCH': 405,
+            'DELETE': 405,
+            'HEAD': 405,
+            'OPTIONS': 405,
+            'TRACE': 405
+        }
+        self.kwargs = {'admin_id': 5}
