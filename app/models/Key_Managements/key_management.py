3fac16a66b4d285896f3d8c7d22baf8b6da0df54
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-14 00:01:37 -0400

    added comment

--- app/models/Key_Managements/key_management.py
+++ app/models/Key_Managements/key_management.py
@@ -15,7 +15,8 @@ class KeyManagement(models.Model):
     def __str__(self):
         return self.iv + " for user " + self.user_id.__str__()
 
-    iv = models.CharField(max_length=255)
+    iv = models.CharField(
+        max_length=255)  # needs to be a 16byte string so please use binascii.hexlify(Random.new().read(8))
     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
