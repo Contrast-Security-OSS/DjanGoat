e79493ace223e5f7e2a661cd61c42422303ac082
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-14 13:11:22 -0400

    added pylint-django to work with linting

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -eu
 
 pip install pylint
-pylint app -f json > pylint_app.json && pylint pygoat -f json > pylint_pygoat.json || echo "lint failed continuing with build"
+pip install pylint-django
+pylint app pygoat -f json > pylint_app.json || echo "lint failed continuing with build"
