580cb23dc91b64e3c9570949ff8570a80145f039
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-16 17:18:36 -0400

    wrote original test

---
+++ app/tests/vulnerabilities/test_a7_missing_access_control.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, RequestFactory, Client
+from app.models import User
+from app.views import sessions_views as sessions
+import pytz
+import datetime
+
+
+class TestMissingFunctionLevelAccessControl(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+
+    def test_route_post(self):
+        # Create user in database
+        input_email = "ryan.dens@contrastsecurity.com"
+        input_password = "12345"
+        input_admin = False
+        input_first_name = "Ryan"
+        input_last_name = "Dens"
+        u_input_create_date = pytz.utc.localize(datetime.datetime(2017, 6, 1, 0, 0))
+        u_input_update_date = pytz.utc.localize(datetime.datetime(2017, 6, 3, 0, 0))
+        model = User.objects.create(
+            email=input_email, password=input_password,
+            is_admin=input_admin, first_name=input_first_name,
+            last_name=input_last_name, created_at=u_input_create_date,
+            updated_at=u_input_update_date
+        )
+
+        factory_request = self.factory.post('/sessions/')
+        factory_response = sessions.sessions_index(factory_request, email="ryan.dens@contrastsecurity.com",
+                                                   password="12345", path="/admin/1/dashboard/")
+        self.assertEqual(factory_response.status_code, 302)
+
+        client_request = self.client.post('/sessions/',
+                                          {'email': 'ryan.dens@contrastsecurity.com', 'password': '12345',
+                                           'path': '/admin/1/dashboard/'},
+                                          follow=True)
+        self.assertEqual(client_request.status_code, 200)
--- app/views/sessions/views.py
+++ app/views/sessions/views.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
+from app.models.User.user import User
 
 
 @require_http_methods(["GET"])
@@ -15,8 +16,34 @@ def logout(request):
 
 
 @require_http_methods(["GET", "POST"])
-def sessions_index(request):
-    return HttpResponse('You are at the sessions index')
+def sessions_index(request, email=None, password=None, path='/dashboard/home'):
+    if request.method == "POST":
+
+        # Set path variable
+        if 'path' in request.POST:
+            path = request.POST['path']
+
+        # Set email variable
+        if 'email' in request.POST:
+            email = request.POST['email']
+        elif email is None:
+            return HttpResponse("Error: no email inputted")
+
+        # Set password variable
+        if 'password' in request.POST:
+            password = request.POST['password']
+        elif password is None:
+            return HttpResponse("Error: no password inputted")
+
+        try:
+            response = HttpResponseRedirect(path)
+            user = User.authenticate(email, password)
+            response.set_cookie("auth_token", user.auth_token)
+            return response
+        except User.DoesNotExist:
+            return HttpResponse("User does not exist!")
+
+    return HttpResponse("Sessions Index")
 
 
 @require_http_methods(["GET"])
