fd144b9fd5abc4ea4135e622c6e4c86b0124ca5e
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-20 15:06:50 -0400

    CONTRAST-15036 added decrypt bank account number card on pay index template

---
+++ static/app/javascript/users/pay/index.js
@@ -0,0 +1,9 @@
+message = "For your safety your account number is stored encrypted \
+as well as presented to you in an encrypted form\n\nFor your convenience \
+, you can decrypt your bank account number at any time using our \
+conveniently located decryption function"
+
+function encryptedBankNum() {
+
+    alert(message)
+}
