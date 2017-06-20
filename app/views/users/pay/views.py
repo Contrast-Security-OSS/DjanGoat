f88f99ff350b3082d6bd2dfb4e0da3c338212455
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-06-20 10:51:10 -0400

    CONTRAST-15036 added css files for user pay index, created first form

--- app/views/users/pay/views.py
+++ app/views/users/pay/views.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.template.loader import get_template
 
 
 @require_http_methods(["POST"])
@@ -16,7 +17,8 @@ def decrypt_bank_acct_num(request, user_id):
 
 @require_http_methods(["GET", "POST"])
 def user_pay_index(request, user_id):
-    return HttpResponse("Pay index for user " + str(user_id))
+    template = get_template('users/pay/index.html')
+    return HttpResponse(template.render())
 
 
 @require_http_methods(["GET"])
