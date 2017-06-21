7a7d65b7dfe0da02dfa49e72f95bd5d3c6d19e26
Galen Palmer <galen.palmer@contrastsecurity.com>
2017-06-21 12:34:41 -0400

    index template updated for python

--- app/views/users/messages/views.py
+++ app/views/users/messages/views.py
@@ -3,19 +3,44 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render, redirect
+from django.utils import timezone
 
 from app.decorators import user_is_authenticated
 from app.models import User
 from app.models import Message
+from app.views import utils
 
 
 @require_http_methods(["GET", "POST"])
 # @user_is_authenticated
 def user_messages(request, user_id):
-    return render(request, "users/messages/index.html", { 'current_user': None })
+    # current_user = utils.current_user(request)
+    current_user = User.objects.first()
+
+    if request.method == "GET":
+        return render(request, "users/messages/index.html", {
+            'current_user': current_user,
+            'available_recipients': User.objects.all()
+        })
+    else:
+        Message.objects.create(creator_id=int(request.POST['creator_id']),
+                               receiver_id=int(request.POST['receiver_id']),
+                               message=request.POST['message'],
+                               read=int(request.POST['read']),
+                               created_at=str(timezone.now()))
+        return redirect(request, "users/"+ current_user.id +"/messages")
 
 
 @require_http_methods(["GET", "DELETE"])
 # @user_is_authenticated
 def user_message(request, user_id, message_id):
-    return render(request, "users/messages/show.html", { 'current_user': None })
+    # current_user = utils.current_user(request)
+    current_user = User.objects.first()
+
+    message = Message.objects.get(message_id)
+    if request.method == "GET":
+        return render(request, "users/messages/show.html", { 'current_user': current_user, 'message': message })
+    else:
+        message.delete()
+        return redirect(request, "users/"+ current_user.id +"/messages")
+
