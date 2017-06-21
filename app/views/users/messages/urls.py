b945d7db52ec4f48e1a391e4d6e5cbb74fce70fa
Galen Palmer <galen.palmer@contrastsecurity.com>
2017-06-21 12:53:35 -0400

    resolving test failures

--- app/views/users/messages/urls.py
+++ app/views/users/messages/urls.py
@@ -4,5 +4,6 @@ import views as messages_views
 
 urlpatterns = [
     url(r'^$', messages_views.user_messages, name="user_messages"),
-    url(r'^(?P<message_id>[0-9]+)$', messages_views.user_message, name="user_message")
+    url(r'^(?P<message_id>[0-9]+)$', messages_views.user_message,
+        name="user_message")
 ]
--- app/views/users/messages/views.py
+++ app/views/users/messages/views.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render, redirect
 from django.utils import timezone
@@ -12,10 +11,9 @@ from app.views import utils
 
 
 @require_http_methods(["GET", "POST"])
-# @user_is_authenticated
+@user_is_authenticated
 def user_messages(request, user_id):
-    # current_user = utils.current_user(request)
-    current_user = User.objects.first()
+    current_user = utils.current_user(request)
 
     if request.method == "GET":
         return render(request, "users/messages/index.html", {
@@ -29,19 +27,20 @@ def user_messages(request, user_id):
                                read=int(request.POST['read']),
                                created_at=str(timezone.now()),
                                updated_at=str(timezone.now()))
-        return redirect("/users/"+ str(current_user.id) +"/messages")
+        return redirect("/users/" + str(current_user.id) + "/messages")
 
 
 @require_http_methods(["GET", "DELETE"])
-# @user_is_authenticated
+@user_is_authenticated
 def user_message(request, user_id, message_id):
-    # current_user = utils.current_user(request)
-    current_user = User.objects.first()
+    current_user = utils.current_user(request)
 
     message = Message.objects.get(message_id)
     if request.method == "GET":
-        return render(request, "users/messages/show.html", { 'current_user': current_user, 'message': message })
+        return render(request, "users/messages/show.html", {
+            'current_user': current_user,
+            'message': message
+        })
     else:
         message.delete()
-        return redirect("/users/"+ str(current_user.id) +"/messages")
-
+        return redirect("/users/" + str(current_user.id) + "/messages")
