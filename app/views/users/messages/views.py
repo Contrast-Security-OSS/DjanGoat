44839ec058cb60538aea89a8f7a80088fdd3ac4d
Galen Palmer <galen.palmer@contrastsecurity.com>
2017-06-21 11:28:05 -0400

    CONTRAST-15032 initial copy of templates from railsgoat

--- app/views/users/messages/views.py
+++ app/views/users/messages/views.py
@@ -2,17 +2,20 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.shortcuts import render, redirect
 
 from app.decorators import user_is_authenticated
+from app.models import User
+from app.models import Message
 
 
 @require_http_methods(["GET", "POST"])
-@user_is_authenticated
+# @user_is_authenticated
 def user_messages(request, user_id):
-    return HttpResponse(str(user_id) + "Messages Index!")
+    return render(request, "users/messages/index.html", { 'current_user': None })
 
 
 @require_http_methods(["GET", "DELETE"])
-@user_is_authenticated
+# @user_is_authenticated
 def user_message(request, user_id, message_id):
-    return HttpResponse("show user message" + str(user_id) + str(message_id))
+    return render(request, "users/messages/show.html", { 'current_user': None })
