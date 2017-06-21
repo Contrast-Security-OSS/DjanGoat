b700d5037207af6f771cdf0d907dc1f5d59a2817
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-21 17:40:26 -0400

    CONTRAST 15035 added download ability

--- app/views/users/benefit_forms/views.py
+++ app/views/users/benefit_forms/views.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
-from django.shortcuts import render
-
+from django.shortcuts import render, reverse
+from wsgiref.util import FileWrapper
 from app.decorators import user_is_authenticated
+from pygoat.settings import BASE_DIR
+import os
 
 
 @require_http_methods(["GET", "POST"])
@@ -36,7 +38,12 @@ def user_benefit_form(request, user_id, benefit_form_id):
 @require_http_methods(["GET"])
 @user_is_authenticated
 def download(request):
-    return HttpResponse("Download user benefit form")
+    path = BASE_DIR + r"/" + request.GET.get('name')
+    wrapper = FileWrapper(file(path))
+    response = HttpResponse(wrapper, content_type='application/pdf')  # hard coded for now
+    response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(path)
+    response['Content-Length'] = os.path.getsize(path)
+    return response
 
 
 @require_http_methods(["POST"])
