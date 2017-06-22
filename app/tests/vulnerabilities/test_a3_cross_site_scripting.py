36c1d6a6b07a8e5fc178af981ca2f842b3ab79d6
Alice Duan <aliceduan@Alices-MacBook-Pro.local>
2017-06-22 16:18:15 -0400

    CONTRAST-14981 passes this test by adding a form to the page

--- app/tests/vulnerabilities/test_a3_cross_site_scripting.py
+++ app/tests/vulnerabilities/test_a3_cross_site_scripting.py
@@ -9,11 +9,10 @@ import json
 
 class CrossSiteScriptingTest(WebTest):
     def setUp(self):
-        self.param = {'email': 'catdog@gmail.com', 'first_name': '<script>alert(1)</script>',
+        self.param = {'email': 'catdog@gmail.com', 'first_name': '<form>dogs</form>',
                       'last_name': 'dog', 'password': '123456',
                       'confirm': '123456'}
         page = self.app.get('/signup/')
-        self.assertEqual(len(page.forms), 1)
         form = page.forms[0]
         form.set('email', self.param['email'])
         form.set('first_name', self.param['first_name'])
@@ -22,11 +21,9 @@ class CrossSiteScriptingTest(WebTest):
         form.set('confirm', self.param['confirm'])
         self.form = form
 
+    #
     def test_header_xss(self):
-        response = self.form.submit()
-        print (response)
-        user = User.objects.filter(email=self.param['first_name'])
-        self.assertTrue('<script>alert(1)</script>' in response)
-        self.assertNotContains('&lt;script&gt;alert(1)&lt;/script&gt;' not in response)
-        user.first().delete()
+        self.form.submit()
+        page = self.app.get('/dashboard/home')
+        self.assertEqual(len(page.forms), 1)
 
