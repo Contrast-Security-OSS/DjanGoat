8cc755c7e3b6eaec167ab1c4617c65cbf5bbda81
Alice Duan <aliceduan@Alices-MacBook-Pro.local>
2017-06-22 16:50:48 -0400

    CONTRAST-14981 added two more tests

--- app/tests/vulnerabilities/test_a3_cross_site_scripting.py
+++ app/tests/vulnerabilities/test_a3_cross_site_scripting.py
@@ -9,21 +9,39 @@ import json
 
 class CrossSiteScriptingTest(WebTest):
     def setUp(self):
-        self.param = {'email': 'catdog@gmail.com', 'first_name': '<form>dogs</form>',
+        self.param = {'email': 'catdog@gmail.com',
                       'last_name': 'dog', 'password': '123456',
                       'confirm': '123456'}
         page = self.app.get('/signup/')
         form = page.forms[0]
         form.set('email', self.param['email'])
-        form.set('first_name', self.param['first_name'])
         form.set('last_name', self.param['last_name'])
         form.set('password', self.param['password'])
         form.set('confirm', self.param['confirm'])
         self.form = form
 
-    #
-    def test_header_xss(self):
+    # the first name field above is specified as a form, so by testing whether
+    # there is a form on the dashboard page, we can confirm that the xss attack
+    # executed correctly
+    def test_header_xss_0_forms(self):
+        first_name = 'dog'
+        self.form.set('first_name', first_name)
+        self.form.submit()
+        page = self.app.get('/dashboard/home')
+        self.assertEqual(len(page.forms), 0)
+
+    def test_header_xss_1_form(self):
+        first_name = '<form action="/action_page.php">First name:<br><input type="text" name="firstname" value="dog"><br><br><input type="submit" value="Submit"></form>'
+        self.form.set('first_name', first_name)
         self.form.submit()
         page = self.app.get('/dashboard/home')
         self.assertEqual(len(page.forms), 1)
+
+    def test_header_xss_2_forms(self):
+        first_name = '<form>dogs</form><form>cat</form>'
+        self.form.set('first_name', first_name)
+        self.form.submit()
+        page = self.app.get('/dashboard/home')
+        self.assertEqual(len(page.forms), 2)
+
 
