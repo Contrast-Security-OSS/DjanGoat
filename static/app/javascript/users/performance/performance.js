f171362ad800f832b3162c05b8dac669da9cea4a
Alice Duan <aliceduan@Alices-MacBook-Pro.local>
2017-06-23 10:47:24 -0400

    CONTRAST-15113 better options, still have to bring to different file and get dates correct

--- static/app/javascript/users/performance/performance.js
+++ static/app/javascript/users/performance/performance.js
@@ -1,28 +1,64 @@
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart']});
+//// Load the Visualization API and the corechart package.
+//google.charts.load('current', {'packages':['corechart']});
+//
+//// Set a callback to run when the Google Visualization API is loaded.
+//google.charts.setOnLoadCallback(drawChart);
+//
+//// Callback that creates and populates a data table,
+//// instantiates the pie chart, passes in the data and
+//// draws it.
+//function drawChart() {
+//
+//    // Create the data table.
+//    var data = new google.visualization.DataTable();
+//    data.addColumn('string', 'PTO');
+//    data.addColumn('number', 'num');
+//    data.addRows([
+//      ['PTO taken', 4],
+//      ['PTO not taken', 1],
+//    ]);
+//
+//    // Set chart options
+//    var options = {
+//        'title':'Available PTO',
+//        'legend':'none',
+//        'slices': {
+//            0: { color: 'red' },
+//            1: { color: '#dddddd' }
+//        },
+//        'titlePosition': 'none',
+//        'backgroundColor': 'transparent',
+//        'pieHole': 0.85,
+//        'pieSliceText': 'value',
+//        'pieSliceTextStyle' : {
+//            color: 'black',
+//        },
+//        'pieSliceBorderColor': 'transparent'
+//    };
+//
+//    // Instantiate and draw our chart, passing in some options.
+//    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+//
+//    chart.draw(data, options);
+//}
 
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart() {
+                  google.charts.load('current', {'packages':['corechart']});
+                  google.charts.setOnLoadCallback(drawChart);
 
-    // Create the data table.
+                  function drawChart() {
 
-                    var data = google.visualization.arrayToDataTable([
-                      ['Year', 'Sales', 'Expenses'],
-        [2016, 1777, 1929]
-                    ]);
+                  var data = google.visualization.arrayToDataTable([
+                        ['Year', 'Score']
+                        {% for review in performance.all %}
+                        , [ {{ review.date_submitted.year }}, {{ review.score }} ]
+                        {% endfor %}
+                        ]);
 
-    // Set chart options
                     var options = {
                       legend: { position: 'right' }
                     };
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-    chart.draw(data, options);
-});
+                    var chart = new google.visualization.LineChart(document.getElementById('line_chart'));
+                    chart.draw(data, options);
+                  }
