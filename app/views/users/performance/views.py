4606cab6ebb3cc98f7f3c997f4ef05f2e973cca4
Alice Duan <aliceduan@Alices-MacBook-Pro.local>
2017-06-23 09:52:08 -0400

    CONTRAST-15113 layout is there, chart displays with fake data

--- app/views/users/performance/views.py
+++ app/views/users/performance/views.py
@@ -2,30 +2,17 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.template.loader import get_template
+from app.views import utils
 from app.decorators import user_is_authenticated
 
 
 @require_http_methods(['GET', 'POST'])
 @user_is_authenticated
 def user_performance_index(request, user_id):
-    return HttpResponse("Usr" + str(user_id) + "performance index")
-
-
-@require_http_methods(["GET"])
-@user_is_authenticated
-def new_user_performance(request, user_id):
-    return HttpResponse("New user performance for user " + str(user_id))
-
-
-@require_http_methods(["GET"])
-@user_is_authenticated
-def edit_user_performance(request, user_id, performance_id):
-    return HttpResponse("Edit performance " + str(performance_id) +
-                        " for user " + str(user_id))
-
-
-@require_http_methods(["GET", "PATCH", "PUT", "DELETE"])
-@user_is_authenticated
-def user_performance(request, user_id, performance_id):
-    return HttpResponse("View performance " + str(performance_id) +
-                        " for user " + str(user_id))
+    current_user = utils.current_user(request)
+    performance = current_user.u_id
+    t = get_template('users/performance/index.html')
+    html = t.render({'performance': performance,
+                     'current_user': current_user})
+    return HttpResponse(html)
