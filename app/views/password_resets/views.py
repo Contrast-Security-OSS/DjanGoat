1658a276a27ba4c3949ab76d66a36b4822a117c3
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-27 12:39:57 -0400

    basic view for forgot password that allows you to send email via form

--- app/views/password_resets/views.py
+++ app/views/password_resets/views.py
@@ -1,12 +1,26 @@
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_http_methods
+from django.contrib import messages
+from app.models import User
+from django.core.mail import send_mail
 
 
 @require_http_methods(["POST"])
 def forgot_password(request):
-    return HttpResponse('You forgot your password! PANICCCCCC')
+    if len(User.objects.filter(email=request.POST['email'])) > 0:
+        user = User.objects.filter(email=request.POST['email']).first()
+        messages.success(request,
+                         'An email was sent to your email to reset your password!')
+    else:
+        messages.error(request, 'We do not have the email in our system')
+
+    return HttpResponseRedirect('/login')
+
+
+def password_reset_mailer(request, user):
+    return "dsd"
 
 
 # Handler that depending on request type delegated to confirm_token
