78e20026f35b8f6f963ed22ce3898de012160636
Ryan Dens <ryan.dens@contrastsecurity.com>
2017-09-06 16:16:51 -0400

    addeed basic changes to engine

--- pygoat/settings.py
+++ pygoat/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'pygoat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db.psql'),
     }
 }
 
