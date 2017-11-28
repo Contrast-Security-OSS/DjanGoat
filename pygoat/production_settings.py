c10c5e61ab2ab04d63b7ccece986206a530700f0
Alex Boyd <alex.boyd@contrastsecurity.com>
2017-11-28 12:28:10 -0500

    Fix Prod Settings to default

--- pygoat/production_settings.py
+++ pygoat/production_settings.py
@@ -1,9 +1,9 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DjanGoat',
-        'USER': 'djangoat',
-        'PASSWORD': 'default1!',
+        'NAME': 'INSERT_NAME',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',  # can leave blank (sets to default)
         'PORT': '',  # can leave blank (sets to default)
     }
