e6a882946cbf0ebd96cae40a60563ba41a21fd2b
Alex Boyd <alex.boyd@contrastsecurity.com>
2018-12-13 12:39:18 -0500

    switch db settings to in-memory sqlite

--- pygoat/production_settings.py
+++ pygoat/production_settings.py
@@ -3,9 +3,6 @@ import dj_database_url
 
 connection_string = os.environ.get('MYSQL_HOST', None)
 
-if not connection_string:
-    print('NO CONNECTION STRINGGGGG')
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
