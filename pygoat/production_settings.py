85dd9739032e81f6103a15d92e5fa20c87f7cbdf
Alex Boyd <alex.boyd@contrastsecurity.com>
2018-12-13 12:23:59 -0500

    add ability to connect to db via connection string

--- pygoat/production_settings.py
+++ pygoat/production_settings.py
@@ -1,10 +1,11 @@
+import os
+import dj_database_url
+
+connection_string = os.environ.get('MYSQL_HOST', None)
+
+if not connection_string:
+    print('NO CONNECTION STRINGGGGG')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',  # can leave blank (sets to default)
-        'PORT': '',  # can leave blank (sets to default)
-    }
+    'default': dj_database_url.parse(connection_string, conn_max_age=600)
 }
